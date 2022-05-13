893699aa0c4da4cb301c20872ce89d25c5ca1deb
Simon Scholz <opensource.simon@gmail.com>
2022-05-13 22:53:30 +0200

    Change bg image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { SEO } from '../components/SEO'
 import { HomepageQuery } from '../types/graphql'
 import { CategoryCard } from '../components/cards/CategoryCard'
 import { GatsbyImage } from 'gatsby-plugin-image'
-import bgImage from '../assets/img/son-coding-with-daddy-opacity.webp'
+import bgImage from '../assets/img/me-presenting-bg.webp'
 
 export const query = graphql`
 	query HomepageAndAvatarQuery {
@@ -61,81 +61,82 @@ function Homepage({ data }: Props): React.ReactElement {
 			<SEO title="Home" />
 
 			<div
-				className="hidden md:flex flex-row rounded-b-lg bg-blue-500 bg-no-repeat bg-cover bg-center bg-opacity-80"
+				className="bg-blue-500 bg-no-repeat bg-cover bg-center bg-opacity-80 p-4"
 				style={{ backgroundImage: `url(${bgImage})` }}
 			>
-				<div className="flex-row flex-grow mt-2 p-10 text-white">
-					<div className="flex-shrink w-2/3 p-6">
-						Hi,
-						<br /> I love <span className="font-bold">open source</span> and to{' '}
-						<span className="font-bold">share</span> my knowledge.
-						<br /> (... and deleting legacy code with my son 😜)
-						<br />
-						<br />
-						In the future my intention is to create YouTube programming
-						videos...
-						<br />
-						So stay tuned... 😉
-						<br />
+				<div className="hidden md:flex flex-row ">
+					<div className="md:w-1/6 p-2">
+						<GatsbyImage
+							image={data!.me!.childImageSharp!.gatsbyImageData}
+							alt="Me"
+							className="rounded-full m-3"
+						/>
+					</div>
+					<div className="flex-row flex-grow mt-2 p-6 text-white">
+						<div className="flex-shrink w-2/3 p-4">
+							Hi,
+							<br /> I love <span className="font-bold">open source</span> and
+							to <span className="font-bold">share</span> my knowledge.
+							<br />
+							<br />
+							In the future my intention is to create YouTube programming
+							videos...
+							<br />
+							So stay tuned... 😉
+							<br />
+						</div>
 					</div>
 				</div>
-				<div className="md:w-1/5 p-2">
-					<GatsbyImage
-						image={data!.me!.childImageSharp!.gatsbyImageData}
-						alt="Me"
-						className="rounded-full m-3"
+
+				<div className="container w-2/3 grid justify-items-stretch gap-2 grid-cols-1 md:grid-cols-2">
+					<CategoryCard
+						id="1"
+						title="Java, Kotlin, JVM, Testing"
+						description="Tutorials on how to use these JVM languages"
+						gatsbyImageData={data!.jvm!.childImageSharp!.gatsbyImageData}
+						path="/tutorials"
+					/>
+
+					<CategoryCard
+						id="1"
+						title="Spring Boot"
+						description="Tutorials about Spring Boot applications"
+						gatsbyImageData={data!.springBoot!.childImageSharp!.gatsbyImageData}
+						path="/tutorials"
+					/>
+
+					<CategoryCard
+						id="1"
+						title="Quarkus"
+						description="Tutorials about Quarkus applications"
+						gatsbyImageData={data!.quarkus!.childImageSharp!.gatsbyImageData}
+						path="/tutorials"
+					/>
+
+					<CategoryCard
+						id="1"
+						title="Web Frontend Development"
+						description="Tutorials about React JS, TypeScript, Gatsby"
+						gatsbyImageData={data!.webDev!.childImageSharp!.gatsbyImageData}
+						path="/tutorials"
+					/>
+
+					<CategoryCard
+						id="1"
+						title="Eclipse RCP"
+						description="Tutorials about the Eclipse IDE and Eclipse Rich Client Platform"
+						gatsbyImageData={data!.eclipse!.childImageSharp!.gatsbyImageData}
+						path="/tutorials"
+					/>
+
+					<CategoryCard
+						id="1"
+						title="Tools and Technology"
+						description="General Sections about tools and technology, e.g., git, GitHub and more"
+						gatsbyImageData={data!.gears!.childImageSharp!.gatsbyImageData}
+						path="/tutorials"
 					/>
 				</div>
-			</div>
-
-			<div className="container mx-auto grid justify-items-stretch gap-2 grid-cols-1 md:grid-cols-3">
-				<CategoryCard
-					id="1"
-					title="Java, Kotlin, JVM, Testing"
-					description="Tutorials on how to use these JVM languages"
-					gatsbyImageData={data!.jvm!.childImageSharp!.gatsbyImageData}
-					path="/tutorials"
-				/>
-
-				<CategoryCard
-					id="1"
-					title="Spring Boot"
-					description="Tutorials about Spring Boot applications"
-					gatsbyImageData={data!.springBoot!.childImageSharp!.gatsbyImageData}
-					path="/tutorials"
-				/>
-
-				<CategoryCard
-					id="1"
-					title="Quarkus"
-					description="Tutorials about Quarkus applications"
-					gatsbyImageData={data!.quarkus!.childImageSharp!.gatsbyImageData}
-					path="/tutorials"
-				/>
-
-				<CategoryCard
-					id="1"
-					title="Web Frontend Development"
-					description="Tutorials about React JS, TypeScript, Gatsby"
-					gatsbyImageData={data!.webDev!.childImageSharp!.gatsbyImageData}
-					path="/tutorials"
-				/>
-
-				<CategoryCard
-					id="1"
-					title="Eclipse RCP"
-					description="Tutorials about the Eclipse IDE and Eclipse Rich Client Platform"
-					gatsbyImageData={data!.eclipse!.childImageSharp!.gatsbyImageData}
-					path="/tutorials"
-				/>
-
-				<CategoryCard
-					id="1"
-					title="Tools and Technology"
-					description="General Sections about tools and technology, e.g., git, GitHub and more"
-					gatsbyImageData={data!.gears!.childImageSharp!.gatsbyImageData}
-					path="/tutorials"
-				/>
 			</div>
 		</>
 	)
