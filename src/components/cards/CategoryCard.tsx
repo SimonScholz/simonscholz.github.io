5ec0ba09918199c279e8c3e69dfb6e16367e5455
Simon Scholz <opensource.simon@gmail.com>
2022-05-14 00:02:56 +0200

    Make container also round with transparent bg

--- src/components/cards/CategoryCard.tsx
+++ src/components/cards/CategoryCard.tsx
@@ -21,11 +21,11 @@ export const CategoryCard = ({
 		<Link to={path} key={id}>
 			<div className="mt-3 mx-3 cursor-pointer border border-gray-400 bg-white bg-opacity-70 rounded-lg p-4 h-12 flex flex-row justify-between hover:shadow-lg  items-center">
 				<div className="text-gray-900 font-bold text-xl">{title}</div>
-				<div className="flex ml-5">
+				<div className="flex ml-5 border border-gray-400 bg-white bg-opacity-70 w-16 h-16 rounded-full">
 					<GatsbyImage
 						image={gatsbyImageData}
 						alt="Category Image"
-						className="w-16 h-16 rounded-full mr-4"
+						className="w-16 h-16 rounded-full"
 					/>
 				</div>
 			</div>
