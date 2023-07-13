5360644af2775cdc0080e556fd21567178029c31
Simon Scholz <opensource.simon@gmail.com>
2023-07-13 21:48:31 +0200

    Fix IntersectionObserver in composable

--- src/composables/fading-intersection.ts
+++ src/composables/fading-intersection.ts
@@ -1,23 +1,27 @@
-const observerOptions = {
-  root: null,
-  threshold: 0.1,
-  rootMargin: '0px 0px -50px 0px',
-}
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show')
-      observer.unobserve(entry.target)
-    }
-  })
-}, observerOptions)
-
 export function useFadeIn(elementsCallback: () => Element[]) {
+  const observerOptions = {
+    root: null,
+    threshold: 0.1,
+    rootMargin: '0px 0px -50px 0px',
+  }
+
+  const disconnectCallback = ref<null | { disconnect: () => void }>(null)
+
   onMounted(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show')
+          observer.unobserve(entry.target)
+        }
+      })
+    }, observerOptions)
+    disconnectCallback.value = { disconnect: observer.disconnect }
     elementsCallback().forEach((element) => {
       observer.observe(element)
     })
   })
-  onUnmounted(observer.disconnect)
+  onUnmounted(() => {
+    disconnectCallback?.value?.disconnect()
+  })
 }
