8989264f51200c24468208e333fcd697ac36fe6f
Simon Scholz <opensource.simon@gmail.com>
2024-04-30 10:48:01 +0200

    Adjust Skill section

--- components/cv/SpecialSkills.vue
+++ components/cv/SpecialSkills.vue
@@ -14,12 +14,13 @@ import medal from "~/assets/medal.png";
     <div class="flex flex-col mt-3">
       <h2 class="font-semibold">Technologies / Frameworks</h2>
       <span>
-        Spring (Spring Boot 2 incl. Webflux and Spring Cloud), Quarkus IO, Flutter, Android,
+        Spring (Spring Boot 3 incl. Webflux and Spring Cloud), Quarkus IO, Flutter, Android,
         Vue, Nuxt, React JS, Gatsby, NextJS, Eclipse RCP (Rich Client Platform), SWT/JFace/Databinding, JavaFX/AWT/Swing,
         JPA/Hibernate/EclipseLink, Junit 5, AssertJ/Assertk,
         Mockito, Mockk, Hamcrest, Spock, RxJava/Project Reactor IO/Mutiny, Resilience4J, ArrowKt, Ktor,
         Kubernetes, Terraform, GitOps/Flux, Ansible, Keycloak, Micrometer Metrics, Inbluxdb, Stackdriver,
-        Prometheus, Grafana, Swagger / OAS, OpenAPI Generator, GCP (Google Cloud Platform)
+        Prometheus, Grafana, Open Telemetry, Jaeger, Swagger / OAS, OpenAPI Generator, Zally, GCP (Google Cloud Platform),
+        Google PubSub, Apache Kafka, ActiveMQ
       </span>
     </div>
     <div class="flex flex-col mt-3">
